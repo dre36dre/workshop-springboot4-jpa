767a9b686f493612402995d95162f0c62e43ff10
Anderson freires <andersonfreires1979@gmail.com>
2026-06-03 15:11:00 -0300

    Project created

---
+++ src/main/java/com/andersonfreires/spring/SpringbootCourseApplication.java
@@ -0,0 +1,13 @@
+package com.andersonfreires.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCourseApplication.class, args);
+	}
+
+}
