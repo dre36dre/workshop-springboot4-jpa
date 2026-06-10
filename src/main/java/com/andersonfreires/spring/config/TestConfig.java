9d48f3989e31c6ff84ead9b3375764ffa7fde5f4
Anderson freires <andersonfreires1979@gmail.com>
2026-06-10 17:12:15 -0300

    OrderItem, many-to-many association with extra attributes PART 1

--- src/main/java/com/andersonfreires/spring/config/TestConfig.java
+++ src/main/java/com/andersonfreires/spring/config/TestConfig.java
@@ -1,7 +1,6 @@
 package com.andersonfreires.spring.config;
 
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
