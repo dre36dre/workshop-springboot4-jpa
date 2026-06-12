5908c6befbcf2d4d6bc357b1ecf1e2a425e48bb5
Anderson freires <andersonfreires1979@gmail.com>
2026-06-12 11:17:12 -0300

    Product, one-to-one association

--- src/main/java/com/andersonfreires/spring/entities/pk/OrderItemPK.java
+++ src/main/java/com/andersonfreires/spring/entities/pk/OrderItemPK.java
@@ -1,7 +1,7 @@
 package com.andersonfreires.spring.entities.pk;
 
+
 import java.io.Serializable;
-import java.util.Objects;
 
 import com.andersonfreires.spring.entities.Order;
 import com.andersonfreires.spring.entities.Product;
@@ -11,19 +11,17 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 @Embeddable
-public class OrderItemPK  implements Serializable{
-
+public class OrderItemPK implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@ManyToOne
-	@JoinColumn(name="order_id")
+	@JoinColumn(name = "order_id")
 	private Order order;
-	
+
 	@ManyToOne
-	@JoinColumn(name="product_id")
+	@JoinColumn(name = "product_id")
 	private Product product;
-	
-	
+
 	public Order getOrder() {
 		return order;
 	}
@@ -36,10 +34,16 @@ public class OrderItemPK  implements Serializable{
 	public void setProduct(Product product) {
 		this.product = product;
 	}
+
 	@Override
 	public int hashCode() {
-		return Objects.hash(order, product);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((order == null) ? 0 : order.hashCode());
+		result = prime * result + ((product == null) ? 0 : product.hashCode());
+		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -49,9 +53,16 @@ public class OrderItemPK  implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		OrderItemPK other = (OrderItemPK) obj;
-		return Objects.equals(order, other.order) && Objects.equals(product, other.product);
+		if (order == null) {
+			if (other.order != null)
+				return false;
+		} else if (!order.equals(other.order))
+			return false;
+		if (product == null) {
+			if (other.product != null)
+				return false;
+		} else if (!product.equals(other.product))
+			return false;
+		return true;
 	}
-	
-	
-	
 }
