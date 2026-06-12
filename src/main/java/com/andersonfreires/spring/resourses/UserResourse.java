e258285731c5205acb9a3080690fcb8a01b81240
Anderson freires <andersonfreires1979@gmail.com>
2026-06-12 12:49:22 -0300

    User delete

--- src/main/java/com/andersonfreires/spring/resourses/UserResourse.java
+++ src/main/java/com/andersonfreires/spring/resourses/UserResourse.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +18,6 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.andersonfreires.spring.entities.User;
 import com.andersonfreires.spring.services.UserService;
 
-import jakarta.servlet.Servlet;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
@@ -46,5 +46,11 @@ public class UserResourse {
 	return ResponseEntity.created(uri).body(obj) ;
 	}
 	
+	
+	@DeleteMapping(value= "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 	
 }
